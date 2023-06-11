4e803524a29ab5286f555b1aae89da432cbd0959
Matheus <matheus.marcolino.1320642@sga.pucminas.br>
2023-06-11 18:54:11 -0300

    final

--- graph.py
+++ graph.py
@@ -1,10 +1,11 @@
+import itertools
 import sys
 from random import randint
 class Graph:
     def __init__(_self, adjacency_matrix: list[list], cost_list: list[list], k: int):
         _self.adjacency_matrix = adjacency_matrix
         #_self.costs = cost_list
-        _self.k = k
+        _self.k = int(k)
         
     def _floyd_warshall(_self):
         n = len(_self.adjacency_matrix)
@@ -33,18 +34,51 @@ class Graph:
     def k_centers_aproximado(_self):
         distances = _self._floyd_warshall()
         centers = []
-        initial = randint(1, len(_self.adjacency_matrix))
+        initial = randint(1, (len(_self.adjacency_matrix) -1))
         centers.append(initial)
         _self._find_centers(centers, distances)
         dists = []
+        final_dists = []
         smaller = sys.maxsize
         for i in  range (1, len(distances)):
             for center in centers:
-                if (distances[i][center] < smaller and i != center):
+                if (distances[i][center] < smaller and not i in centers):
                     smaller = distances[i][center]
-            dists.append(smaller)
+                    dists.append(smaller)
+            if(not i in centers):
+                dists.sort()
+                final_dists.append(dists[0])
+                dists.clear()
+                smaller = sys.maxsize
         dists.sort()
-        print(dists[(len(dists) -1)])
+        print(final_dists[(len(final_dists) -1)])
         print(centers)
         
-    #def k_centers_preciso(_self):
+
+    def _calcular_raio(_self, centros, matrix):
+        raio = 0
+        for i in range (1, len(_self.adjacency_matrix)):
+            menor_distancia = sys.maxsize
+            for c in centros:
+                if (c != i):
+                    distancia = matrix[i][c]
+                    if distancia < menor_distancia:
+                        menor_distancia = distancia
+            if menor_distancia > raio:
+                    raio = menor_distancia
+        return raio
+
+    def k_centros_exato(_self):
+        distances = _self._floyd_warshall()
+        menor_raio = sys.maxsize
+        melhores_centros = []
+
+        comb_centros = itertools.combinations(range(1, len(_self.adjacency_matrix)), _self.k)
+        for centros in comb_centros:
+            raio = _self._calcular_raio(centros, distances)
+            if raio < menor_raio:
+                menor_raio = raio
+                melhores_centros = centros
+
+        print(melhores_centros)
+        print(menor_raio)
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from reader import Reader
 from graph import Graph
-reader = Reader("C:/Users/mathe/Downloads/pmed1.txt")
-graph = Graph(reader.lista_de_adjancenia, reader.costs, reader.k)
-graph.k_centers_preciso()
+import time
+
+option = -1
+path = ''
+while option != 0:
+    option = int(input("Insira 1 caso queira passar um path de grafo ou 0 para finalizar: "))
+    if option == 1:
+        path = input("Insira o caminho (as barras devem ser assim /): ")
+    elif option == 0:
+        break
+    else:
+        continue
+    reader = Reader(path)
+    graph = Graph(reader.lista_de_adjancenia, reader.costs, reader.k)
+    method_option = -1
+    while method_option != 0:
+        method_option = int(input("Insira 1 caso queira o método aproximado, 2 para o preciso ou 0 para finalizar: "))
+        if method_option == 1:
+            start = time.time()
+            graph.k_centers_aproximado()
+            end = time.time()
+            print("Tempo de execucação: ", (end - start) * 10**3, "ms")
+        elif method_option == 2:
+            start = time.time()
+            graph.k_centros_exato()
+            end = time.time()
+            print("Tempo de execucação: ", (end - start) * 10**3, "ms")
+        elif method_option == 0:
+            break
+        else:
+            continue
